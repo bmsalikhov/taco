a487cbc9dee409e6881011704edae37c182f5a69
Bulat Salikhov <bmsalikhov@gmail.com>
2024-02-23 22:32:41 +0300

    - удалил HomeController.java - добавил обработку home в WebConfig - добавил Converter для ингредиентов тако - добавил OrderController - добавил валидацию форм составления тако/оформления заказа - добавил orderForm.html и design.html

--- src/main/java/ru/bmsalikhov/taco/controller/DesignTacoController.java
+++ src/main/java/ru/bmsalikhov/taco/controller/DesignTacoController.java
@@ -1,12 +1,11 @@
 package ru.bmsalikhov.taco.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import ru.bmsalikhov.taco.Ingredient;
 import ru.bmsalikhov.taco.Ingredient.Type;
 import ru.bmsalikhov.taco.Taco;
@@ -56,6 +55,20 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco,
+                              Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
