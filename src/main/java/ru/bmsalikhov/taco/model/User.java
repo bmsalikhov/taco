42f65f135e514ccd6439c59c2b981ba235f66350
Bulat Salikhov <bmsalikhov@gmail.com>
2024-02-26 23:29:52 +0300

    - добавил зависимость spring-boot-starter-security - добавил класс SecurityConfig - добавил зависимость spring-boot-starter-data-jpa - добавил класс User

---
+++ src/main/java/ru/bmsalikhov/taco/model/User.java
@@ -0,0 +1,53 @@
+package ru.bmsalikhov.taco.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+public class User implements UserDetails  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
